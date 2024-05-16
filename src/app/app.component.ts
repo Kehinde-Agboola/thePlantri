15e220a09f353dfe912c1dde50de422638994483
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-05-16 17:54:09 +0100

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { PageFooterComponent } from './layout/page-footer/page-footer.component';
+import { PageHeaderComponent } from './layout/page-header/page-header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [PageHeaderComponent, PageFooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'thePlantri';
