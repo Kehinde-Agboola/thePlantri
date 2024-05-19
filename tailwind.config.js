90f91429d954a8862cb444710cd9818a6ede2da0
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-05-20 00:14:22 +0100

    added-color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         navs: "#232323",
         hero: "#F6F4EE",
         para: "#333333",
+        text: "#C5C5C5",
       },
       fontFamily: {
         rate: ["Montserrat", "sans-serif"],
