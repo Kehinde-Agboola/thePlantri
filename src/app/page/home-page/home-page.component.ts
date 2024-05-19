0c83ee8d0c8c1984afdba28f99ecf63d4155cd8e
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-05-20 00:13:43 +0100

    added-service-offer-component

--- src/app/page/home-page/home-page.component.ts
+++ src/app/page/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './components/hero/hero.component';
+import { ServiceOfferComponent } from './components/service-offer/service-offer.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeroComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
+  imports: [HeroComponent, ServiceOfferComponent],
 })
 export class HomePageComponent {}
