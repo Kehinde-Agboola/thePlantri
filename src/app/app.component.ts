206cc5a3d3d7fc16cd48676524abfe9861a2eb5b
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-05-20 03:15:21 +0100

    nothing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PageFooterComponent } from './layout/page-footer/page-footer.component';
 import { PageHeaderComponent } from './layout/page-header/page-header.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PageHeaderComponent, PageFooterComponent, RouterOutlet],
+  imports: [
+    CommonModule,
+    PageHeaderComponent,
+    PageFooterComponent,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
