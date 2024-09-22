f2a5b30d09c0c968d98e60747c526e38b0de0b5e
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-09-23 00:32:47 +0100

    collection-page

---
+++ src/app/page/collection-page/collection-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './collection-page.component.html',
+  styleUrl: './collection-page.component.scss'
+})
+export class CollectionPageComponent {
+
+}
---
+++ src/app/page/collection-page/collection-page.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CollectionPageRoutingModule } from './collection-page-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CollectionPageRoutingModule
+  ]
+})
+export class CollectionPageModule { }
